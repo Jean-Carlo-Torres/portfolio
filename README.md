7075002236dc75efe4d779c7788b30e039724d09
Jean Carlo <jeansanto2002@gmail.com>
2023-08-10 14:10:45 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Portfólio
+
+<p align="center">Meu Portfólio, onde falo sobre mim, demostro as tecnologias que eu trabalho, minhas formações, especialidades e projetos pessoais. </p>
+
+<h6 align="center"><a href="#">**Link para testar o projeto**</a></h6>
+
+<img src="./assets/img/tela-principal.png">
+
+## Tecnologias utilizadas no projeto
+* HTML
+* CSS
+* JavaScript
+
+
+[![Linkedin Badge](https://img.shields.io/badge/-JeanCarlo-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/jeancarlotorre619b/)](https://www.linkedin.com/in/jeancarlotorre619b/)
+
+<hr>
+<h3>Contribuindo</h3>
+
+
+⭐️ Star o projeto
+
+🐛 Encontrar e relatar issues
---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
+
+    });
+
+
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPauser: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: false
+            }
+        }
+    });
+
+
+});
+$('.menu-btn').click(function () {
+    $('.navbar .menu').toggleClass("active");
+    $('.menu-btn i').toggleClass("active");
+});
