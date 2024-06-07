e62eb0d16a114bf32512a0e012c7461d97827173
Jean Carlo <jeansanto2002@gmail.com>
2024-06-07 20:08:18 -0300

    Aplicando modificações no titulo e sessão sobre

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,3 +37,20 @@ $('.menu-btn').click(function () {
     $('.navbar .menu').toggleClass("active");
     $('.menu-btn i').toggleClass("active");
 });
+
+/*====== scroll reveal ==========*/
+
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+  })
+  
+  ScrollReveal().reveal('.home-content, .heading', { origin: 'top' })
+  ScrollReveal().reveal(
+    '.home-img, .service-container, .portfolio-box, .contact',
+    { origin: 'bottom' }
+  )
+  ScrollReveal().reveal('.home-content h1, .about-img ', { origin: 'left' })
+  ScrollReveal().reveal('.home-content p, .about-content ', { origin: 'right' })
